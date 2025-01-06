2ae8fc4518f3c6b78146b60367e2eb13c25191bc
Oliver Montalván <olivermontalvanm@gmail.com>
2025-01-06 00:31:35 -0600

    Added endpoints to get all items and all measure units

--- catalogController.js
+++ catalogController.js
@@ -11,6 +11,8 @@ class ProjectController {
 
         this.router.get( "/projects", [ hasToken ], this.getAllProjectOptions.bind( this ) );
         this.router.get( "/roles", [ hasToken ], this.getAllRoleOptions.bind( this ) );
+        this.router.get( "/items", [ hasToken ], this.getAllItemOptions.bind( this ) );
+        this.router.get( "/measureUnits", [ hasToken ], this.getAllMeasureUnitOptions.bind( this ) );
     }
 
     async getAllProjectOptions( req, res ) {
@@ -19,9 +21,39 @@ class ProjectController {
             
             const projects = await CatalogService.getAllProjectOptions( req.user );
 
-            if( !projects ) throw new Error( "Could not get logged user" );
+            if( !projects ) throw new Error( "Could not get projects" );
 
             return res.status( 200 ).json( projects );
+        } catch ( e ) {
+            console.error( e );
+            return res.status( 500 ).json( { message: "Internal server error" } );
+        }
+    }
+
+    async getAllItemOptions( req, res ) {
+        try {
+            if( !req.user ) return res.status( 401 ).json( { message: "Unauthorized" } );
+            
+            const items = await CatalogService.getAllItemOptions( );
+
+            if( !items ) throw new Error( "Could not get items" );
+
+            return res.status( 200 ).json( items );
+        } catch ( e ) {
+            console.error( e );
+            return res.status( 500 ).json( { message: "Internal server error" } );
+        }
+    }
+
+    async getAllMeasureUnitOptions( req, res ) {
+        try {
+            if( !req.user ) return res.status( 401 ).json( { message: "Unauthorized" } );
+            
+            const measures = await CatalogService.getAllMeasureUnitOptions( );
+
+            if( !measures ) throw new Error( "Could not get items" );
+
+            return res.status( 200 ).json( measures );
         } catch ( e ) {
             console.error( e );
             return res.status( 500 ).json( { message: "Internal server error" } );
